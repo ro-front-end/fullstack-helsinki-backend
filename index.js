ef30559eadf3cdaeb95e266ca33a5470b36363d9
ro-front-end <rockfrontend@outlook.com>
2025-03-13 20:16:03 -0600

    changes

--- index.js
+++ index.js
@@ -1,85 +1,30 @@
 const express = require("express");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+// Middleware para analizar JSON
 app.use(express.json());
-app.use(express.static("dist"));
-app.use(cors());
 
+// Sirve los archivos estáticos desde la carpeta "dist"
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Ruta de ejemplo para la API
 let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    important: true,
-  },
-
-  {
-    id: 3,
-    content: " GET and POST are the most importajnt methods of HTTP protocol",
-    important: true,
-  },
+  { id: 1, content: "HTML is easy", important: true },
+  { id: 2, content: "Browser can execute only JavaScript", important: false },
 ];
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-
-  return maxId + 1;
-};
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+// Maneja todas las demás rutas enviando el archivo "index.html"
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  };
-
-  notes = notes.concat(note);
-
-  response.json(note);
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
