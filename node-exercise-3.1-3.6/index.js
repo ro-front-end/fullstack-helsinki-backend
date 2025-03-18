94993cc94e6851f7820e1cd29e96b2c489e8b4ca
ro-front-end <rockfrontend@outlook.com>
2025-03-18 12:14:16 -0600

    exercises part3

---
+++ node-exercise-3.1-3.6/index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+const generateId = () => {
+  persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+
+  return maxId + 1;
+};
+
+const date = new Date();
+const formmatedDate = date.toString();
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === id);
+
+  response.json(person);
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    response.status(400).json({
+      error: "missing content",
+    });
+  }
+
+  const nameExists = persons.some((person) => person.name === body.name);
+  if (nameExists) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    important: Boolean(body.importat) || false,
+    id: generateID(),
+  };
+
+  persons.push(newPerson);
+
+  response.status(201).json(newPerson);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.get("/api/persons/info", (request, response) => {
+  response.send(
+    `<h3>Phonebook has info for ${persons.length}</h3><p>${formmatedDate}</p>`
+  );
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unkwon endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
